a92334a438900b7cfc02dc9eeb3b361e25439731
Antonio_PC <a.hermosin.garcia@gmail.com>
2023-03-08 17:43:49 +0100

    ,

--- Proyecto_Cartitas/src/controlador/gestion_BD.java
+++ Proyecto_Cartitas/src/controlador/gestion_BD.java
@@ -17,8 +17,8 @@ public class gestion_BD {
      * Crea la conexion a la BD y establece autoCommit a false
      */
     public static void crearConexion(){
-        try {                                       //TODO poner la base de datos bien
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/BBDD", "root", "");
+        try {                                       
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "");
             con.setAutoCommit(false);
             System.out.println("Conexion creada");
         } catch (SQLException e) {
@@ -35,56 +35,69 @@ public class gestion_BD {
         try {
             st=con.createStatement();
             sql="CREATE DATABASE IF NOT EXISTS `mpt_db` CHARACTER SET utf8;\n";
-            st.addBatch(sql);
-
+            st.execute(sql);
+            
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mpt_db", "root", "");
+            con.setAutoCommit(false);
+            
             st=con.createStatement();
-            sql="CREATE DATABASE IF NOT EXISTS `mpt_db` CHARACTER SET utf8;\n";
+            
+            /*sql="USE mpt_db;";
+            st.execute(sql);*/
+            
+            st.clearBatch();
+
+            //Importante los " IF NOT EXISTS " porque si no va a tirar excepciones
+            sql="""
+                CREATE TABLE IF NOT EXISTS `cartas` (
+                `IdCarta` int(11) NOT NULL,
+                `Nombre` varchar(30) NOT NULL,
+                `Ataque` int(11) NOT NULL,
+                `Vida` int(11) NOT NULL,
+                `Descripcion` varchar(150) NOT NULL,
+                `Rareza` int(11) NOT NULL,
+                `URL_Imagen` varchar(250) NOT NULL,
+                PRIMARY KEY (`IdCarta`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+                """;
             st.addBatch(sql);
 
-            sql="CREATE TABLE `cartas` (\n"+
-                "`IdCarta` int(11) NOT NULL,\n"+
-                "`Nombre` varchar(30) NOT NULL,\n"+
-                "`Ataque` int(11) NOT NULL,\n"+
-                "`Vida` int(11) NOT NULL,\n"+
-                "`Descripcion` varchar(150) NOT NULL,\n"+
-                "`Rareza` int(11) NOT NULL,\n"+
-                "`URL_Imagen` varchar(250) NOT NULL,\n"+
-                "PRIMARY KEY (`IdCarta`)\n"+
-              ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;\n";
+            sql="""
+                CREATE TABLE IF NOT EXISTS `usuario` (
+                `IdUsuario` int(11) NOT NULL AUTO_INCREMENT,
+                `Nombre` varchar(30) NOT NULL UNIQUE,
+                `Pwd` varchar(30) NOT NULL,
+                `Edad` int(11) NOT NULL,
+                `Monedas` int(11) NOT NULL,
+                `Region` varchar(6) NOT NULL,
+                PRIMARY KEY (`IdUsuario`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+                """;
             st.addBatch(sql);
 
-            sql="CREATE TABLE `usuario` (\n"+
-                "`IdUsuario` int(11) NOT NULL,\n"+
-                "`Nombre` varchar(30) NOT NULL,\n"+
-                "`Pwd` varchar(30) NOT NULL,\n"+
-                "`Edad` int(11) NOT NULL,\n"+
-                "`Monedas` int(11) NOT NULL,\n"+
-                "`Region` varchar(6) NOT NULL,\n"+
-                "PRIMARY KEY (`IdUsuario`)\n"+
-              ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;\n";
+            sql="""
+                CREATE TABLE IF NOT EXISTS `coleccion` (
+                `IdColeccion` int(11) NOT NULL,
+                `IdCarta` int(11) NOT NULL,
+                `IdUsuario` int(11) NOT NULL,
+                `NombreCarta` varchar(30) NOT NULL,
+                PRIMARY KEY (`IdColeccion`),
+                UNIQUE KEY `IdUsuario` (`IdUsuario`) USING BTREE,
+                KEY `IdCarta` (`IdCarta`),
+                CONSTRAINT `coleccion_FK` FOREIGN KEY (`IdCarta`) REFERENCES `cartas` (`IdCarta`),
+                CONSTRAINT `coleccion_FK_1` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`IdUsuario`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+                """;
             st.addBatch(sql);
 
-            sql="CREATE TABLE `coleccion` (\n"+
-                "`IdColeccion` int(11) NOT NULL,\n"+
-                "`IdCarta` int(11) NOT NULL,\n"+
-                "`IdUsuario` int(11) NOT NULL,\n"+
-                "`NombreCarta` varchar(30) NOT NULL,\n"+
-                "PRIMARY KEY (`IdColeccion`),\n"+
-                "UNIQUE KEY `IdUsuario` (`IdUsuario`) USING BTREE,\n"+
-                "KEY `IdCarta` (`IdCarta`),\n"+
-                "CONSTRAINT `coleccion_FK` FOREIGN KEY (`IdCarta`) REFERENCES `cartas` (`IdCarta`),\n"+
-                "CONSTRAINT `coleccion_FK_1` FOREIGN KEY (`IdUsuario`) REFERENCES `usuario` (`IdUsuario`)\n"+
-              ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;\n";
-            st.addBatch(sql);
-
-            int [] numUpdates=st.executeBatch();
-			for (int i = 0; i < numUpdates.length; i++) {
-				if (numUpdates[i] == Statement.SUCCESS_NO_INFO) {
-					System.out.println("Execution " + i + ": unknown number of rows updated");
-				} else {
-					System.out.println("Execution " + i + "successful: " + numUpdates[i] + " rows updated");
-				}
-			}
+            /*int [] numUpdates=*/st.executeBatch();
+//			for (int i = 0; i < numUpdates.length; i++) {
+//				if (numUpdates[i] == Statement.SUCCESS_NO_INFO) {
+//					System.out.println("Execution " + i + ": unknown number of rows updated");
+//				} else {
+//					System.out.println("Execution " + i + "successful: " + numUpdates[i] + " rows updated");
+//				}
+//			}
             con.commit();
             //cerramos el statement este.
             st.close();
@@ -110,29 +123,31 @@ public class gestion_BD {
         String sql;
         try {
             st=con.createStatement();
-            sql="INSERT INTO\n"+
-            "cartas(idCarta,nombre,descripcion,ataque,vida,rareza,url_imagen)\n"+
-            "VALUES\n"+
-            "(1,'Rey','Larga vida al rey... O no.',1,4,3,'src/resources/img/rey_card.png'),\n"+
-            "(2,'Reina','Mi padre quería unir los reinos.',4,1,3,'src/resources/img/reina_card.png'),\n"+
-            "(3,'Bufón','Tener contento al castillo es importante.',2,7,1,'src/resources/img/bufon_card.png'),\n"+
-            "(4,'Payaso','La plebe necesita entretenimiento.',2,6,1,'src/resources/img/payaso_card.png'),\n"+
-            "(5,'Caballero','Defender y morir por el reino.',7,9,2,'src/resources/img/caballero_card.png'),\n"+
-            "(6,'Verdugo','Los malhechores deben ser castigados',7,4,2,'src/resources/img/verdugo_card.png'),\n"+
-            "(7,'Granjero','No me pises lo sembrado.',3,5,1,'src/resources/img/granjero_card.png'),\n"+
-            "(8,'Herrero','Esa espada está un poco mellada.',6,6,1,'src/resources/img/herrero_card.png'),\n"+
-            "(9,'Sacerdote','Por la fe de Dios.',1,7,1,'src/resources/img/sacerdote_card.png'),\n"+
-            "(10,'Cultista','¿Niños raptados? Pregunta al sacerdote.',8,6,1,'src/resources/img/cultista_card.png'),\n"+
-            "(11,'Ángel','Hay que combatir el mal.',6,7,2,'src/resources/img/angel_card.png'),\n"+
-            "(12,'Demonio','Ser malo es divertido',7,6,2,'src/resources/img/demonio_card.png'),\n"+
-            "(13,'Mago','El conocimiento es poder.',9,2,2,'src/resources/img/mago_card.png'),\n"+
-            "(14,'Druida','Sí, aquel oso era yo',8,8,2,'src/resources/img/druida_card.png'),\n"+
-            "(15,'Alquimista','¿Quieres una poción de la risa?',4,7,1,'src/resources/img/alquimista_card.png'),\n"+
-            "(16,'Cazador','Solía ser un caballero, pero una flecha me dio en la rodilla.',7,5,1,'src/resources/img/cazador.png'),\n"+
-            "(17,'Pirata','La X me trajo hasta aquí',5,3,1,'src/resources/img/pirata_card.png'),\n"+
-            "(18,'Elfo','¿Por qué vivir si no es para amar?',3,10,3,'src/resources/img/elfo_card.png'),\n"+
-            "(19,'Orco','¡Tiempo de matar!',10,7,3,'src/resources/img/orco_card.png'),\n"+
-            "(20,'Viajero del Tiempo','Pues sí que todo esto era campo...',10,10,4,'src/resources/img/viajero_card.png');\n";
+            sql="""
+                INSERT INTO
+                cartas(idCarta,nombre,descripcion,ataque,vida,rareza,url_imagen)
+                VALUES
+                (1,'Rey','Larga vida al rey... O no.',1,4,3,'src/resources/img/rey_card.png'),
+                (2,'Reina','Mi padre quer\u00eda unir los reinos.',4,1,3,'src/resources/img/reina_card.png'),
+                (3,'Buf\u00f3n','Tener contento al castillo es importante.',2,7,1,'src/resources/img/bufon_card.png'),
+                (4,'Payaso','La plebe necesita entretenimiento.',2,6,1,'src/resources/img/payaso_card.png'),
+                (5,'Caballero','Defender y morir por el reino.',7,9,2,'src/resources/img/caballero_card.png'),
+                (6,'Verdugo','Los malhechores deben ser castigados',7,4,2,'src/resources/img/verdugo_card.png'),
+                (7,'Granjero','No me pises lo sembrado.',3,5,1,'src/resources/img/granjero_card.png'),
+                (8,'Herrero','Esa espada est\u00e1 un poco mellada.',6,6,1,'src/resources/img/herrero_card.png'),
+                (9,'Sacerdote','Por la fe de Dios.',1,7,1,'src/resources/img/sacerdote_card.png'),
+                (10,'Cultista','\u00bfNi\u00f1os raptados? Pregunta al sacerdote.',8,6,1,'src/resources/img/cultista_card.png'),
+                (11,'\u00c1ngel','Hay que combatir el mal.',6,7,2,'src/resources/img/angel_card.png'),
+                (12,'Demonio','Ser malo es divertido',7,6,2,'src/resources/img/demonio_card.png'),
+                (13,'Mago','El conocimiento es poder.',9,2,2,'src/resources/img/mago_card.png'),
+                (14,'Druida','S\u00ed, aquel oso era yo',8,8,2,'src/resources/img/druida_card.png'),
+                (15,'Alquimista','\u00bfQuieres una poci\u00f3n de la risa?',4,7,1,'src/resources/img/alquimista_card.png'),
+                (16,'Cazador','Sol\u00eda ser un caballero, pero una flecha me dio en la rodilla.',7,5,1,'src/resources/img/cazador.png'),
+                (17,'Pirata','La X me trajo hasta aqu\u00ed',5,3,1,'src/resources/img/pirata_card.png'),
+                (18,'Elfo','\u00bfPor qu\u00e9 vivir si no es para amar?',3,10,3,'src/resources/img/elfo_card.png'),
+                (19,'Orco','\u00a1Tiempo de matar!',10,7,3,'src/resources/img/orco_card.png'),
+                (20,'Viajero del Tiempo','Pues s\u00ed que todo esto era campo...',10,10,4,'src/resources/img/viajero_card.png');
+                """;
             st.addBatch(sql);
             
             int [] numUpdates=st.executeBatch();
@@ -199,21 +214,31 @@ public class gestion_BD {
      * Da de alta un nuevo usuario
      */
     public static boolean altaUsuario(String nombre, String pwd, int edad, String reg){
+        System.out.println("Alta usuario");
         boolean registrado = false;
         try {
-            PreparedStatement pst = con.prepareStatement("INSERT INTO usuario (Nombre, Pwd, Edad, Monedas, Region) VALUES ?, ?, ?, 1000, ?");
+            System.out.println(con.getMetaData().getDatabaseProductName());
+            PreparedStatement pst = con.prepareStatement("INSERT INTO mpt_db.usuario (Nombre, Pwd, Edad, Monedas, Region) VALUES (?, ?, ?, 1000, ?)");
             
             pst.setString(1, nombre);
             pst.setString(2, pwd);
             pst.setInt(3, edad);
             pst.setString(4, reg);
+            System.out.println(pst.toString());
+            
+            con.setAutoCommit(true); //TODO Si no está esta línea, no funciona (por algun motivo se ralla que flipas con el executeUpdate y el autocommit false)
             
             int ejecutado = pst.executeUpdate();
-            
+            System.out.println(ejecutado);
             if (ejecutado == 1) {
                 registrado = true;
+                System.out.println("DEBERIA INSERTAR");
             }
+            
+            con.setAutoCommit(false);
+            
         } catch (SQLException e) {
+            e.printStackTrace();
         }
         return registrado;
     }//altaUsuario
@@ -227,7 +252,7 @@ public class gestion_BD {
         //TODO Select de las cartas
         
         try {
-            PreparedStatement st = con.prepareStatement("SELECT DE LAS CARTAS DEL JUGADOR");
+            PreparedStatement st = con.prepareStatement("SELECT * FROM cartas JOIN coleccion USING(IdCarta) WHERE IdUsuario = (SELECT IdUsuario FROM usuario WHERE Nombre = ?) ");
             st.setString(1, nombreJugador);
             
             ResultSet rs = st.executeQuery();
